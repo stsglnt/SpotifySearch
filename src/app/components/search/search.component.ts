250b9fe1dd7c10c749094789098c6345ce9bc754
Stanislav Galiant <stsglnt@gmail.com>
2017-02-16 15:57:25 +0200

    Angular 2 spotify search app

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../../Artist';
+
+@Component({
+  moduleId: module.id,
+  selector: 'search',
+  templateUrl: 'search.component.html',
+  
+})
+export class SearchComponent {
+    searchStr: string;    
+    private searchRes: Artist[];
+
+    constructor(private _spotifyService: SpotifyService){
+
+    }
+    searchMusic () {
+        this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+            console.log(res);
+            this.searchRes = res.artists.items;
+        })
+    }
+}
